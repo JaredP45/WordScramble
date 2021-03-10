aff410ea0a536f4ff40097a566f7d3126aa319fb
Jared <paubelj45@gmail.com>
2021-03-10 08:09:35 -0600

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by student on 3/10/21.
+//  Copyright © 2021 IN185 BS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
